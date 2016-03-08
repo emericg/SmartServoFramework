608c3e22532b0bdc7ad5b737e8296c2a745f2d7d
Emeric Grange <emeric.grange@gmail.com>
2016-03-08 21:49:59 +0100

    Change license of the examples, they have no reason to be LGPL

--- examples/ex_advance_scanner.cpp
+++ examples/ex_advance_scanner.cpp
@@ -1,26 +1,30 @@
 /*!
- * This file is part of SmartServoFramework.
- * Copyright (c) 2014, INRIA, All rights reserved.
+ * The MIT License (MIT)
  *
- * SmartServoFramework is free software; you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as published by
- * the Free Software Foundation; either version 3 of the License, or
- * (at your option) any later version.
+ * Copyright (c) 2014 INRIA
  *
- * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Lesser General Public License for more details.
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
  *
- * You should have received a copy of the GNU Lesser General Public License
- * along with this software. If not, see <http://www.gnu.org/licenses/lgpl-3.0.txt>.
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
  *
  * \file ex_advance_scanner.cpp
  * \date 27/05/2014
  * \author Emeric Grange <emeric.grange@inria.fr>
- */
-
-/*
+ *
  * Scan serial ports for Dynamixel servos, for all IDs ([0:253]) and all serial port speeds.
  * You can choose what serial speed you want to use for the scan through '-start' and '-stop'
  * arguments. Note: baudrate(BPS) = 2000000 / (Data + 1)
